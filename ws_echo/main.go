3dfdb1dd00afa1c4718629b6621de366e7a12acb
masashi.g <masashi.g@gmail.com>
2019-02-19 22:29:28 +0900

    simple websocket echo

---
+++ ws_echo/main.go
@@ -0,0 +1,32 @@
+// simple echo server with websocket
+
+package main
+
+import (
+    "io"
+    "net/http"
+
+    "golang.org/x/net/websocket"
+)
+
+// Echo the data received on the WebSocket.
+func EchoServer(ws *websocket.Conn) {
+    io.Copy(ws, ws)
+}
+
+// This example demonstrates a trivial echo server.
+func main() {
+    http.Handle("/", websocket.Handler(EchoServer)) // REFUSED
+    /*
+    http.HandleFunc("/echo",
+        func(w http.ResponseWriter, req *http.Request) {
+            s := websocket.Server{Handler: websocket.Handler(echoHandler)}
+            s.ServeHTTP(w, req)
+        }
+    )
+    */
+    err := http.ListenAndServe(":3001", nil)
+    if err != nil {
+        panic("ListenAndServe: " + err.Error())
+    }
+}
